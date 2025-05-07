ae0a660a2d5e2fdade66a3f11100c170da022ea1
JAMureR <jmuresat@gmail.com>
2025-05-07 17:12:14 +0100

    Peticiones Ajax, para guardar likes y dislikes en la base de datos

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,28 +1,60 @@
-var script = document.createElement('script');  // Necesario para hacer funcionar jQuery si no está incluido
+var script = document.createElement('script');
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.head.appendChild(script);
 
-window.addEventListener("load", function() {
+script.onload = function () {
+    window.addEventListener("load", function () {
 
-    // Establecer el cursor
-    $(document).on('mouseover', '.btn-like, .btn-dislike', function(){
-        $(this).css('cursor', 'pointer');
+        // Establecer el cursor
+        $(document).on('mouseover', '.btn-like, .btn-dislike', function () {
+            $(this).css('cursor', 'pointer');
+        });
+
+        function like() {
+            $(document).on('click', '.btn-like', function () {
+                console.log('like');
+                $(this).addClass('btn-dislike').removeClass('btn-like');
+                $(this).attr('src', 'img/heart-red.png');
+
+                
+                $.ajax({
+                    url: '/like/' + $(this).data('id'), // (ruta relativa),
+                    type: 'GET',
+                    success: function (response) {
+                        if(response.like){
+                            console.log('Has dado like a la publicacion');
+                        }else{
+                            console.log('Error al dar like');
+                        }
+                        
+                    }
+                });
+            });
+        }
+
+        function dislike() {
+            $(document).on('click', '.btn-dislike', function () {
+                console.log('dislike');
+                $(this).addClass('btn-like').removeClass('btn-dislike');
+                $(this).attr('src', 'img/heart-gray.png');
+
+                $.ajax({
+                    url: '/dislike/' + $(this).data('id'), // (ruta relativa),
+                    type: 'GET',
+                    success: function (response) {
+                        if(response.like){
+                            console.log('Has dado dislike a la publicacion');
+                        }else{
+                            console.log('Error al dar dislike');
+                        }
+                        
+                    }
+                });
+
+            });
+        }
+
+        like();
+        dislike();
     });
-
-    // Botón like
-    $(document).on('click', '.btn-like', function() {
-        console.log('like');
-        $(this).addClass('btn-dislike').removeClass('btn-like');
-        $(this).attr('src', 'img/heart-red.png'); // Revisa esta ruta
-        // Llama a la función dislike si es necesario
-    });
-
-    // Botón dislike
-    $(document).on('click', '.btn-dislike', function() {
-        console.log('dislike');
-        $(this).addClass('btn-like').removeClass('btn-dislike');
-        $(this).attr('src', 'img/heart-gray.png'); // Revisa esta ruta
-        // Llama a la función like si es necesario
-    });
-
-});
+};
