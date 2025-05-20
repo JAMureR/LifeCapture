6dfa9b30909e22502dd059ec2e58e98a6cc2e50f
JAMureR <jmuresat@gmail.com>
2025-05-20 12:50:35 +0100

    Se inicia la funcion de borrar y editar las fotos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,8 @@
 
 
     <div class="py-12">
-        
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 text-black">
                         @foreach($images as $image)
@@ -88,7 +89,8 @@
 
 
                 </div>
-            
+            </div>
+        </div>
 
 
 </x-app-layout>
