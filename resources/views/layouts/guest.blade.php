d59dd09d2c91d7f192b665db36284a20d464b971
JAMureR <jmuresat@gmail.com>
2025-05-28 17:32:12 +0100

    Logo en vistas y en login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js','resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center py-0 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div class="logo-wrapper">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('img/Logo_aplicacion.png') }}" alt="Logo_login" class="logo_login block mb-0" />
                 </a>
             </div>
 
