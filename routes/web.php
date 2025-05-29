14e5d36478af19336860e3ced641db770cf2056e
JAMureR <jmuresat@gmail.com>
2025-05-29 07:14:57 +0100

    Ordenacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,37 @@ Route::get('/dashboard', function () {
 //Rutas Generales
 // Rutas protegidas por autenticación
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+
+    //Home
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+
+    //Usuario
+    Route::get('/perfil/{id}', [UserController::class, 'profile'])->name('profile');
+    Route::get('/personas/{search?}', [UserController::class, 'index'])->name('user.index');
+
+    //Perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/user/avatar/{filename}', [ProfileController::class, 'getImage'])->name('user.avatar');
+
+    //Imagen
     Route::get('/subir-imagen', [ImageController::class, 'create'])->name('image.create');
     Route::post('/image/save', [ImageController::class, 'save'])->name('image.save');
     Route::get('/image/file/{filename}', [ImageController::class, 'getImage'])->name('image.file');
     Route::get('/imagen/{id}', [ImageController::class, 'detail'])->name('image.detail');
-    Route::post('/comment/save', [CommentController::class, 'save'])->name('comment.save');
-    Route::get('/comment/delete/{id}', [CommentController::class, 'delete'])->name('comment.delete');
-    Route::get('/like/{image_id}', [LikeController::class, 'like'])->name('like.save');
-    Route::get('/dislike/{image_id}', [LikeController::class, 'dislike'])->name('like.delete');
-    Route::get('/likes', [LikeController::class, 'index'])->name('likes');
-    Route::get('/perfil/{id}', [UserController::class, 'profile'])->name('profile');
     Route::get('/image/delete/{id}', [ImageController::class, 'delete'])->name('image.delete');
     Route::get('/image/edit/{id}', [ImageController::class, 'edit'])->name('image.edit');
     Route::post('/image/update', [ImageController::class, 'update'])->name('image.update');
-    Route::get('/personas/{search?}', [UserController::class, 'index'])->name('user.index');
+
+    //Comentarios
+    Route::post('/comment/save', [CommentController::class, 'save'])->name('comment.save');
+    Route::get('/comment/delete/{id}', [CommentController::class, 'delete'])->name('comment.delete');
+    Route::get('/like/{image_id}', [LikeController::class, 'like'])->name('like.save');
+
+    //likes
+    Route::get('/dislike/{image_id}', [LikeController::class, 'dislike'])->name('like.delete');
+    Route::get('/likes', [LikeController::class, 'index'])->name('likes');
+    
 });
 
 require __DIR__.'/auth.php';
