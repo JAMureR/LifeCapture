69859371dcc9a75f6e9ec30f0492be20c2c964e5
JAMureR <jmuresat@gmail.com>
2025-06-04 13:05:53 +0100

    Vista likes, cambio publicaciones que te gustan

--- resources/views/like/index.blade.php
+++ resources/views/like/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         @include('includes.message')
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Publicaciones a las que has dado like') }}
+            {{ __('Publicaciones que te gustan') }}
         </h2>
     </x-slot>
 
