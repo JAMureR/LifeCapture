170f2a4871eae13efb97d9249f2ce407ab45e8b3
JAMureR <jmuresat@gmail.com>
2025-07-08 15:57:10 +0100

    prueba cambio repo

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,6 +8,7 @@ class Comment extends Model
 {
     protected $table = 'comments';
     
+    
      //Relacion muchos a uno
      public function user(){
         return $this->belongsTo(\App\Models\User::class, 'user_id');
